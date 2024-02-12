ad0fa512ee7c8babaf4c19823c8f6ceedd47e199
EBN10 <basilemir@gmail.com>
2024-02-12 20:35:57 -0300

    Agregada Pagina de Home

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const express= require('express')
-const app = express()
-const path = require ('path')
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
 
-app.get('/',(req,res) => {
-    res.render('index')
-})
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000,()=>{
-    console.log('El servidor esta corriendo de manera correcta en http://localhost:3000');
-})
+app.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log(
+    "El servidor esta corriendo de manera correcta en http://localhost:3000"
+  );
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+const animations = require("@midudev/tailwind-animations");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-  "./**/*.{html,ejs}"
-],
+  content: ["./**/*.{html,ejs}","./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          '50': '#fefeff',
+          '100': '#e2d3fc',
+          '200': '#c7a8f9',
+          '300': '#ab7ef5',
+          '400': '#9053f2',
+          '500': '#7428ef',
+          '600': '#6120cb',
+          '700': '#4e18a7',
+          '800': '#3b0f82',
+          '900': '#28075e',
+        }
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: [animations,require('flowbite/plugin')],
+  
+};
