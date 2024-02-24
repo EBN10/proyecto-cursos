f04752984d39302dbc87095c35ff88af5c375419
Axel Castaño <axelcastano@proton.me>
2024-02-24 03:31:59 -0300

    Setted views folder

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
